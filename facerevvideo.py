1e92ac0a53f27ed274060ef1368d92d4af572024
ashishpatel-dd1 <81282303+ashishpatel-dd1@users.noreply.github.com>
2021-04-13 18:28:38 +0530

    Add files via upload

---
+++ facerevvideo.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
