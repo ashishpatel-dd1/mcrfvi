a23a87a8c0463623415e4d9b8208d225c0a126e1
ashishpatel-dd1 <81282303+ashishpatel-dd1@users.noreply.github.com>
2021-04-16 17:26:44 +0530

    Update recognize_faces_videofile.py

--- recognize_faces_videofile.py
+++ recognize_faces_videofile.py
@@ -4,7 +4,7 @@ Created on Wed Mar 20 11:51:46 2021
 
 @author: Ashish
 """
-# python faceRecVideoFile.py -e encodings.pickle -i mcrfvi/input/friends_s01e01.mkv -o mcrfvi/output/ex2.mp4 -y 0
+# python recognize_faces_videofile.py -e encodings.pickle -i mcrfvi/input/friends_s01e01.mkv -o mcrfvi/output/ex1_2.mp4 -y 0
 
 import argparse
 import pickle
