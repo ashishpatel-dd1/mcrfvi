b3e0579dde67efea79eab11c99e4cdafb3609016
ashishpatel-dd1 <81282303+ashishpatel-dd1@users.noreply.github.com>
2021-04-23 11:34:03 +0530

    Update faceEncode.py

--- faceEncode.py
+++ faceEncode.py
@@ -11,7 +11,7 @@ from imutils import paths
 import face_recognition
 import argparse
 import pickle
-import cv2
+from cv2 import cv2
 import os
 from sklearn.neighbors import KDTree
 import numpy as np
